1b56912cae60f05119177a855db86442c840e10a
RGabaydullov <rsht123g@gmail.com>
2017-07-11 17:23:09 +0300

    new static Node.js server

--- server.js
+++ server.js
@@ -1,11 +1,55 @@
-var express = require('express'),
-    path = require('path'),
-    app = express();
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-app.set('port', 8080);
-app.use('/', express.static(path.join(__dirname + '/dev')));
+http.createServer(function (request, response) {
+    console.log('request starting...');
 
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
 
-var server = app.listen(app.get('port'), function() {
-    console.log('Webserver started')
-});
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(path.join(__dirname, 'dev', filePath), function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                response.writeHead(404);
+                response.end('Sorry, page wasn\'t found: '+error.code+' ..\n');
+                response.end();
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8080);
+console.log('Server running at http://127.0.0.1:8080/');
